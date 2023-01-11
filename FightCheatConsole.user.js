6cb1790ff530f0a58a9b375bf4b017b8a55d8f58
StormCheatPvP <114351474+StormCheatPvP@users.noreply.github.com>
2023-01-11 17:05:14 -0300

    Create FightCheatConsole.user.js

---
+++ FightCheatConsole.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Fight Cheat Console
+// @version      1.0 beta
+// @description  new cheat
+// @author       StormPVP
+// @match        https://kogama.com.br/page/webgl-frame/
+// @match        https://www.kogama.com/page/webgl-frame/
+// @match        https://friends.kogama.com/page/webgl-frame/
+// @require      https://raw.githubusercontent.com/StormCheatPvP/FightCheatConsole/main/code2.0.js
+// @grant        none
+// ==/UserScript==
